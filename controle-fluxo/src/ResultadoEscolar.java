d56eec29460819e6b738dc1c4d9beaa6a2e9dadd
GeisonFR <geisonfr21@gmail.com>
2024-07-03 19:40:15 -0400

    resultado-escolar

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+        double nota = 8.0;
+
+        if(nota >= 7)
+            System.out.println("Aluno Aprovado, Parabéns!");
+            
+        else 
+            System.out.println("Aluno Reprovado, Quem sab na próxima!");
+
+    }
+}
