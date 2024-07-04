1cb7ef507463dba1a98cecb8f614ce7be8bb204f
GeisonFR <geisonfr21@gmail.com>
2024-07-03 20:22:08 -0400

    resultado-refatorado

---
+++ controle-fluxo/src/ResultadoEscolarRefatorado.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ResultadoEscolarRefatorado {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite sua Nota: ");
+        double nota = sc.nextDouble();
+        
+        String resultado = nota >= 7 ? "Aprovado!" : ((nota  >= 5) && ( nota < 7)) ? "Recuperação!" : "Reprovado!";
+        System.out.println(resultado);
+
+        sc.close();
+    }
+}
+ 
