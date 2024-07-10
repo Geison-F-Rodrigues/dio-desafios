6ce88b2c14b97ccf4847427992c74648d170e5e2
GeisonFR <geisonfr21@gmail.com>
2024-07-09 21:14:20 -0400

    exemplo-while

---
+++ controle-fluxo/src/EstruturasDeRepeticao/ExemploWhile.java
@@ -0,0 +1,27 @@
+package EstruturasDeRepeticao;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+        
+        double mesada = 50.0;
+
+        while (mesada > 0) {
+            Double valorDoce = valorAleatorio();
+            if (valorDoce > mesada) {
+                valorDoce = mesada;
+            }
+
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho.");
+            mesada -= valorDoce;
+            System.out.println("Mesada: " + mesada);
+            System.out.println("");
+        }
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+    }
+
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 15);
+    }
+}
