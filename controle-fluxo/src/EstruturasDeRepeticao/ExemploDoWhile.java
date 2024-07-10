927911f389689b61bd7b50c1c7bc9fc87be5c0cb
GeisonFR <geisonfr21@gmail.com>
2024-07-09 21:15:02 -0400

    exemplo-do-while

---
+++ controle-fluxo/src/EstruturasDeRepeticao/ExemploDoWhile.java
@@ -0,0 +1,23 @@
+package EstruturasDeRepeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do {
+            //executando repetidas vezes até alguém atender
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+
+        System.out.println("Alô !!!");
+    }
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu ? " + atendeu);
+        //negando ato de continuar tocando
+        return ! atendeu;
+    }
+}
