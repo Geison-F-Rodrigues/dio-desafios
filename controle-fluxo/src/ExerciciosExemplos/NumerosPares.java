35484ca205210894a6a48deb9a459ce6c527dbbe
GeisonFR <geisonfr21@gmail.com>
2024-07-22 21:10:21 -0400

    numeros-pares

---
+++ controle-fluxo/src/ExerciciosExemplos/NumerosPares.java
@@ -0,0 +1,14 @@
+package ExerciciosExemplos;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        
+        int numero = 0;
+        int total = 100;
+
+        while (numero < total) {
+            numero += 2;
+            System.out.println(numero);
+        }
+    }
+}
