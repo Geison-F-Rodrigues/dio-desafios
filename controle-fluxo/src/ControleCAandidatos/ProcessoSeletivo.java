141cfc713a13b2ac069f9778a450573823d8b097
GeisonFR <geisonfr21@gmail.com>
2024-08-28 20:12:41 -0400

    processo-seletivo-final

--- controle-fluxo/src/ControleCAandidatos/ProcessoSeletivo.java
+++ controle-fluxo/src/ControleCAandidatos/ProcessoSeletivo.java
@@ -1,10 +1,35 @@
 package ControleCAandidatos;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-       imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) 
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIAZDO COM SUCESSO");
+        }while(continuarTentando && tentativasRealizadas < 3);
+        if (atendeu) 
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else
+        System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MÁXIMO TENTATIVAS " + tentativasRealizadas);
+    }
+    //método auxiliar
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
     static void imprimirSelecionados (){
         String [] candidatos = {"FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO"};
